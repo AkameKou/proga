52d8d9f7c5887e80b2509714357c156d36225ece
AkameKou <akamekoike@gmail.com>
2024-08-04 15:12:17 +0500

    Update proga.cpp

--- proga/proga.cpp
+++ proga/proga.cpp
@@ -4,20 +4,21 @@ int main()
 {
 	int n1, n2;
 	char c;
+	cout << "Enter expression: ";
 	cin >> n1 >> c >> n2;
 	switch (c)
 	{
 	case '+':
-		cout << n1 + n2;
+		cout << "Result is " << n1 + n2;
 		break;
 	case '-':
-		cout << n1 - n2;
+		cout << "Result is " << n1 - n2;
 		break;
 	case '*':
-		cout << n1 * n2;
+		cout << "Result is " << n1 * n2;
 		break;
 	case '/':
-		cout << n1 / n2;
+		cout << "Result is " <<  n1 / n2;
 		break;
 	}
 }
